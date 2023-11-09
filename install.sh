94ff4bccf1df39a0f386612529c77e478313c32d
李明 <ming.li@fftai.com>
2023-11-09 09:41:29 +0800

    commit 1.3.0

---
+++ install.sh
@@ -0,0 +1,15 @@
+rm -rf ~/.rocs_server
+git clone -b v1.3 https://github.com/FFTAI/rocs_server.git ~/.rocs_server1.3.0
+
+cd ~/.rocs_server1.3.0/lib
+sh install_env.sh
+
+cd ~/.rocs_server1.3.0
+rm -rf ./bin*
+wget https://github.com/FFTAI/rocs_server/releases/download/v1.3.0/bin.zip
+unzip bin.zip
+
+cd ~/.rocs_server1.3.0
+rm -rf ./sbin*
+wget https://github.com/FFTAI/rocs_server/releases/download/v1.3.0/sbin.zip
+unzip sbin.zip
