fa38250313238e65eef64db79b9c276331b1ccaf
Jaxxxxx <43429810+Jaxleyy@users.noreply.github.com>
2023-11-09 14:41:12 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,36 +2,17 @@
 
 set -e
 
-rocs_server_dir="rocs_server1.3.0"
+git clone -b v1.3.0 https://github.com/FFTAI/rocs_server.git ~/.rocs_server1.3.0
 
-# 检查目录是否已存在
-if [ -d "$rocs_server_dir" ]; then
-  echo "目录 $rocs_server_dir 已经存在，请删除后再运行脚本。"
-  exit 1
-fi
-
-# Clone rocs_server repository and switch to v1.3.0 branch
-git clone -b v1.3.0 https://github.com/FFTAI/rocs_server.git "$rocs_server_dir"
-
-# Install dependencies in the lib directory
-cd "$rocs_server_dir/lib"
+cd ~/.rocs_server1.3.0/lib
 sh install.sh
 
-# Download and extract bin.zip
-cd "$rocs_server_dir"
+cd ~/.rocs_server1.3.0
 rm -rf ./bin*
-wget -c https://github.com/FFTAI/rocs_server/releases/download/v1.3.0/bin.zip
-unzip -o bin.zip
+wget https://github.com/FFTAI/rocs_server/releases/download/v1.3.0/bin.zip
+unzip bin.zip
 
-# Download and extract sbin.zip
-cd "$rocs_server_dir"
+cd ~/.rocs_server1.3.0
 rm -rf ./sbin*
-wget -c https://github.com/FFTAI/rocs_server/releases/download/v1.3.0/sbin.zip
-unzip -o sbin.zip
-
-if [ ! -d "$rocs_server_dir/bin" ] || [ ! -d "$rocs_server_dir/sbin" ]; then
-  echo "Failed to download and extract necessary files."
-  exit 1
-fi
-
-echo "you are success！"
+wget https://github.com/FFTAI/rocs_server/releases/download/v1.3.0/sbin.zip
+unzip sbin.zip
