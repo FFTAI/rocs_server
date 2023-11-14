0a74111eab14805d5744bb80fe07d63f6f9f6c44
Jaxxxxx <43429810+Jaxleyy@users.noreply.github.com>
2023-11-14 15:02:32 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,7 +2,11 @@
 
 set -e
 
-git clone -b v1.3.0 https://github.com/FFTAI/rocs_server.git ~/.rocs_server1.3.0
+if [ -d ~/.rocs_server1.3.0 ]; then
+    cd ~/.rocs_server1.3.0 && git checkout . && git pull
+else
+    git clone -b v1.3.0 https://github.com/FFTAI/rocs_server.git ~/.rocs_server1.3.0
+fi
 
 cd ~/.rocs_server1.3.0/lib
 sh install.sh
