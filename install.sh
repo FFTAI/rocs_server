7b75af297845b5364479f8bcaee9917b78849b07
Jaxaxx <ming.li@fftai.com>
2024-01-16 15:06:49 +0800

    update rocs-svr-2.0.7

--- install.sh
+++ install.sh
@@ -14,10 +14,12 @@ sudo apt install libunittest++-dev -y
 sudo apt install qtbase5-dev -y
 sudo apt install tmux -y
 sudo apt install wget -y
+sudo apt install liburdfdom-dev
+sudo apt install libtinyxml2-dev
 
 wget -O rocs-libs.deb -q --show-progress https://rocs-1302548221.cos.ap-shanghai.myqcloud.com/libs/rocs-libs_1.0.deb
 
-wget -O rocs-svr.deb -q --show-progress https://rocs-1302548221.cos.ap-shanghai.myqcloud.com/server/rocs-svr_1.3.deb
+wget -O rocs-svr.deb -q --show-progress https://rocs-1302548221.cos.ap-shanghai.myqcloud.com/server/rocs-svr_v2.0.7.deb
 
 wget -O rocs-webots.deb -q --show-progress https://rocs-1302548221.cos.ap-shanghai.myqcloud.com/webots/rocs-webots_1.3.deb
 
