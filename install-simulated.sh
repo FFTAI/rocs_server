b0d97dac0f11b41c6f9e9fd0fde868b6e443be49
Jaxaxx <43429810+Jaxaxx@users.noreply.github.com>
2024-04-02 10:30:09 +0800

    update

--- install-simulated.sh
+++ install-simulated.sh
@@ -2,20 +2,10 @@
 
 set -e
 
-sudo apt update -y
-sudo apt --fix-broken install -y
-sudo apt install hostapd -y
-sudo apt install dnsmasq -y
-sudo apt install cmake -y
-sudo apt install libeigen3-dev -y
-sudo apt install build-essential -y
-sudo apt install cmake-curses-gui -y
-sudo apt install libunittest++-dev -y
-sudo apt install qtbase5-dev -y
-sudo apt install tmux -y
-sudo apt install wget -y
-sudo apt install liburdfdom-dev -y 
-sudo apt install libtinyxml2-dev -y
+sudo apt update 
+sudo apt install --fix-broken -y 
+sudo apt install hostapd dnsmasq cmake libeigen3-dev build-essential cmake-curses-gui libunittest++-dev qtbase5-dev tmux wget liburdfdom-dev libtinyxml2-dev -y
+
 
 wget -O rocs-libs.deb -q --show-progress https://rocs-1302548221.cos.ap-shanghai.myqcloud.com/libs/rocs-libs_1.0.deb
 
